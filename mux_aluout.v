5b4f71fe4389a9e6af2858ecf4be9ab2e6f33a03
Pedro-Case <137705805+Pedro-Case@users.noreply.github.com>
2024-06-04 16:08:00 -0300

    Dois muxs

---
+++ mux_aluout.v
@@ -0,0 +1,33 @@
+module MUX (
+    input wire [2:0] SrcOut,
+    input wire [31:0] Lo,
+    input wire [31:0] Hi,
+    input wire [31:0] LT,
+    input wire [31:0] result,
+    input wire [31:0] orOut,
+    input wire [31:0] shiftOut,
+    output reg [31:0] out
+);
+
+
+    always @(*) begin
+        case (SrcOut)
+            3'b000: 
+                out = Lo;
+            3'b001:
+                out = Hi;
+            3'b010:
+                out = LT;
+            3'b011:
+                out = result;
+            3'b100:
+                out = orOut;
+            3'b101:
+                out = shiftOut;
+                
+            default:
+                out = 32'b00000000000000000000000000000000; // Valor padrão
+        endcase
+    end
+
+endmodule
---
+++ mux_pcsource.v
@@ -0,0 +1,29 @@
+module MUX (
+    input wire [2:0] PCSource,
+    input wire [31:0] jumpAdress,
+    input wire [31:0] result,
+    input wire [31:0] aluOut,
+    input wire [31:0] memData,
+    input wire [31:0] EPCReg,
+    output reg [31:0] out
+);
+
+
+    always @(*) begin
+        case (SrcOut)
+            3'b000: 
+                out = jumpAdress;
+            3'b001:
+                out = result;
+            3'b010:
+                out = aluOut;
+            3'b011:
+                out = memData;
+            3'b100:
+                out = EPCReg;
+            default:
+                out = 32'b00000000000000000000000000000000; // Valor padrão
+        endcase
+    end
+
+endmodule
